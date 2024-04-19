b11ef2e94a815f536f306569f87169f6d86fab70
koockie <56578386+koockie@users.noreply.github.com>
2024-04-19 15:09:03 -0400

    Create counter_2bits.sv
    contador 2 bits

---
+++ Guia3/counter_2bits.sv
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module counter_2bits(
+    input logic clk,     // Entrada de reloj
+    output logic [1:0] count  // Salida del contador de 2 bits
+);
+
+    // Registros para almacenar el valor del contador
+    logic [1:0] counter_reg;
+
+    // Incremento del contador en cada flanco de subida del reloj
+    always_ff @(posedge clk) begin
+        counter_reg <= counter_reg + 1;
+    end
+
+    // Salida del contador
+    assign count = counter_reg;
+
+endmodule
