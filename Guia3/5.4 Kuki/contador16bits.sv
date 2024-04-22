064b4d0a9dddd0e3b738ccceec53dd0f51ccddd6
koockie <56578386+koockie@users.noreply.github.com>
2024-04-22 02:35:12 -0400

    Create contador16bits.sv
    creo contador 16bits

---
+++ Guia3/5.4 Kuki/contador16bits.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+module contador16bits(
+    input logic clk,reset,     // Entrada de reloj
+    output logic [15:0] count  // Salida del contador de 2 bits
+);
+
+    // Registros para almacenar el valor del contador
+    logic [15:0] counter_reg;
+
+    // Incremento del contador en cada flanco de subida del reloj
+    always_ff @(posedge clk) begin
+        if(reset)
+            counter_reg <= 'd0;
+        else
+            counter_reg <= counter_reg + 1;
+    end
+
+    // Salida del contador
+    assign count = counter_reg;
+
+endmodule
