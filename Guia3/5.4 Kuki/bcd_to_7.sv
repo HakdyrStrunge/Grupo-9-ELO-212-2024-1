452afac495dd5c091780fb42444c2f974e55bfea
koockie <56578386+koockie@users.noreply.github.com>
2024-04-22 02:32:25 -0400

    Update bcd_to_7.sv
    cambio de polaridad

--- Guia3/5.4 Kuki/bcd_to_7.sv
+++ Guia3/5.4 Kuki/bcd_to_7.sv
@@ -12,18 +12,24 @@ module bcd_to_7 (
 
 always_comb begin
     case (BCD_in) //ESTO ESTA EN POLARIDAD POSITIVA, HAY QUE NEGAR LA SALIDA PARA FPGA CON POLARIDAD NEGATIVA
-        4'd0: sevenSeg = 7'b1111110; // output is abcdefg
-        4'd1: sevenSeg = 7'b0110000;
-        4'd2: sevenSeg = 7'b1101101;
-        4'd3: sevenSeg = 7'b1111001;
-        4'd4: sevenSeg = 7'b0110011;
-        4'd5: sevenSeg = 7'b1011011;
-        4'd6: sevenSeg = 7'b1011111;
-        4'd7: sevenSeg = 7'b1110000;
-        4'd8: sevenSeg = 7'b1111111;
-        4'd9: sevenSeg = 7'b1110011;
-       
-        default: sevenSeg = 7'bxxxxxxx; // Manejar caso por defecto si es necesario
+        4'd0: sevenSeg = 7'b0000001; // output is abcdefg
+        4'd1: sevenSeg = 7'b1001111;
+        4'd2: sevenSeg = 7'b0010010;
+        4'd3: sevenSeg = 7'b0000110;
+        4'd4: sevenSeg = 7'b1001100;
+        4'd5: sevenSeg = 7'b0100100;
+        4'd6: sevenSeg = 7'b0100000;
+        4'd7: sevenSeg = 7'b0001111;
+        4'd8: sevenSeg = 7'b0000000;
+        4'd9: sevenSeg = 7'b0001100;
+        4'd10: sevenSeg = 7'b0001000;
+        4'd11: sevenSeg = 7'b1100000;
+        4'd12: sevenSeg = 7'b0110001;
+        4'd13: sevenSeg = 7'b1000010;
+        4'd14: sevenSeg = 7'b0110000;
+        4'd15: sevenSeg = 7'b0111000;
+        default: sevenSeg = 7'bxxxxxxx; // en caso de valor no esperado
+        //default: sevenSeg = 7'bxxxxxxx; // Manejar caso por defecto si es necesario
     endcase
 end
 
