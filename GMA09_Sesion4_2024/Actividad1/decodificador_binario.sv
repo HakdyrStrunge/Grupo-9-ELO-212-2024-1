9a8949d2fa2491ba5f84d7a0cef4cf1156542b34
Felipevegaz <83191288+Felipevegaz@users.noreply.github.com>
2024-05-03 15:22:03 -0400

    Update decodificador_binario.sv

--- GMA09_Sesion4_2024/Actividad1/decodificador_binario.sv
+++ GMA09_Sesion4_2024/Actividad1/decodificador_binario.sv
@@ -33,15 +33,16 @@ module decodificador_binario(
     logic AN1;
     logic AN0;
     always_comb begin
-         AN7 = (d == 3'b000) ? 1'b1 : 1'b0; // 1 si es vdd y 0 si es falso
-         AN6 = (d == 3'b001) ? 1'b1 : 1'b0;
-         AN5 = (d == 3'b010) ? 1'b1 : 1'b0;
-         AN4 = (d == 3'b011) ? 1'b1 : 1'b0;  
-         AN3 = (d == 3'b100) ? 1'b1 : 1'b0;
-         AN2 = (d == 3'b101) ? 1'b1 : 1'b0;
-         AN1 = (d == 3'b110) ? 1'b1 : 1'b0;
-         AN0 = (d == 3'b111) ? 1'b1 : 1'b0;
+         AN7 = (d == 3'b000) ? 1'b0 : 1'b1; // 1 si es vdd y 0 si es falso
+         AN6 = (d == 3'b001) ? 1'b0 : 1'b1;
+         AN5 = (d == 3'b010) ? 1'b0 : 1'b1;
+         AN4 = (d == 3'b011) ? 1'b0 : 1'b1;  
+         AN3 = (d == 3'b100) ? 1'b0 : 1'b1;
+         AN2 = (d == 3'b101) ? 1'b0 : 1'b1;
+         AN1 = (d == 3'b110) ? 1'b0 : 1'b1;
+         AN0 = (d == 3'b111) ? 1'b0 : 1'b1;
     end
     always_comb begin
         result = {AN7, AN6, AN5, AN4, AN3, AN2, AN1, AN0};
     end
+endmodule
