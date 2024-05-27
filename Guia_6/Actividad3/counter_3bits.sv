d62355af0780d30fa071bc214cc651b2b10d9fe6
Felipevegaz <83191288+Felipevegaz@users.noreply.github.com>
2024-05-27 14:35:56 -0400

    Update counter_3bits.sv

--- Guia_6/Actividad3/counter_3bits.sv
+++ Guia_6/Actividad3/counter_3bits.sv
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 module counter_3bits(
     input logic clk,reset,     // Entrada de reloj
-    output logic [2:0] count  // Salida del contador de 2 bits
+    output logic count  // Salida del contador de 2 bits
 );
 
     // Registros para almacenar el valor del contador
-    logic [2:0] counter_reg;
+    logic counter_reg;
 
     // Incremento del contador en cada flanco de subida del reloj
     always_ff @(posedge clk) begin
