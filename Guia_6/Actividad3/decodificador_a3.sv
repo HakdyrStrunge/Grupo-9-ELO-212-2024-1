343b2f2d65e84f6f2190e61a865bd4083e1b93fc
Felipevegaz <83191288+Felipevegaz@users.noreply.github.com>
2024-05-27 14:34:08 -0400

    Update decodificador_a3.sv

--- Guia_6/Actividad3/decodificador_a3.sv
+++ Guia_6/Actividad3/decodificador_a3.sv
@@ -2,7 +2,7 @@
 
 
 module decodificador_a3(
-    input logic [2:0] d ,
+    input logic d ,
     //output logic q0,
     //output logic q1,
     //output logic q2,
@@ -17,16 +17,9 @@ module decodificador_a3(
            // 2'b01 : result =8'b1111_1101;
             //2'b10 : result =8'b1111_1011;
             //2'b11: result =8'b1111_0111;
-            3'b000 : res =8'b1111_1110;
-            3'b001 : res =8'b1111_1101;
-            3'b010 : res =8'b1111_1011;
-            3'b011 : res =8'b1111_0111;
-            
-            3'b100 : res =8'b1110_1111;
-            3'b101 : res =8'b1101_1111;
-            3'b110 : res =8'b1011_1111;
-            3'b111 : res =8'b0111_1111;
-            //default :result= 8'b1111_1111;
+            1'b0 : res =8'b1111_1110;
+            1'b1 : res =8'b1111_1101;
+         
             default :res= 8'b1111_1110;
          //q0 = (d == 2'b00) ? 1'b1 : 1'b0; // si es vdd y 0 si es falso
          //q1 = (d == 2'b01) ? 1'b1 : 1'b0;
